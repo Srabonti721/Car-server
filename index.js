da181e5654a169735dc8583aebd8103b776dd6ac
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-08-02 07:50:58 +0600

    added delete method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,11 +32,14 @@ app.get("/cars", async(req, res)=>{
   res.send(result)
  })
 
-
- 
-   // Send a ping to confirm a successful connection
-     client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+app.delete("/cars/:id", async(req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await carCollection.deleteOne(query);
+  res.send(result)
+  console.log(result);
+  
+})
 
 // Dream-car 2zJuEA9CWgv6IcvM
 app.get("/", (req, res)=>{
